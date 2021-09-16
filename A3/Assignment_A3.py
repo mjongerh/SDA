9b81fbded0407191e5d8b5ea5fecfd161b90307f
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2021-09-16 12:07:24 +0200

    u

--- A3/Assignment_A3.py
+++ A3/Assignment_A3.py
@@ -11,26 +11,34 @@ ROOT.gStyle.SetTitleOffset(1.8,"y") # adjust with histogram.GetYaxis().SetTitleO
 
 canv = ROOT.TCanvas("canv","plots for SDA course - ROOT 1", 2400,2400 )
 
-canv.Divide(2,2)  # See link above for documentation of the Divide method.
+canv.Divide(3,3)  # See link above for documentation of the Divide method.
 canv.cd(1)        # Switch to the first sub-canvas.
 
 histogram_of_x1 = ROOT.TH1D("histogram_of_x1","histogram of x1",100, 0, 1 ) # name and title  # 100 bins between 0 and 7000 GeV, since it is now the sum, the range has to be increased beyond 3TeV
 histogram_of_x2 = ROOT.TH1D("histogram_of_x2","histogram of x2",100, 0, 1 )
 histogram_of_u2 = ROOT.TH1D("histogram_of_u2","histogram of u2",100, 0, 1 )
 histogram_of_un = ROOT.TH1D("histogram_of_un","histogram of un",100, 0, 1 )
-Nval = 2
+histogram_of_u1 = ROOT.TH1D("histogram_of_u1","histogram of u1",100, 0, 1 )
+histogram_of_u10 = ROOT.TH1D("histogram_of_u10","histogram of u10",100, 0, 1 )
+
+Nval = 1000
+
 for i in range (5000) :
-    x1 = ROOT.gRandom.Rndm()
-    x2 = ROOT.gRandom.Rndm()
-    u2 = (x1+x2)/2
-    histogram_of_x1.Fill(x1)
-    histogram_of_x2.Fill(x2)
-    histogram_of_u2.Fill(u2)
+    x1 = 0
     Xsum = 0
     for j in range(Nval) :
         Xsum += ROOT.gRandom.Rndm()
-    Xsum /= Nval
-    histogram_of_un.Fill(Xsum)
+        if (j==1) :
+            histogram_of_x1.Fill(Xsum/j)
+            x1 = Xsum
+        if (j==2):
+            histogram_of_u2.Fill(Xsum/j)
+            histogram_of_x2.Fill(Xsum-x1)
+        if (j==10):
+            histogram_of_u10.Fill(Xsum/j)
+        if (j==Nval):
+            histogram_of_un.Fill(Xsum/j)
+
 
 
 histogram_of_x1.Draw()
@@ -48,10 +56,14 @@ MEANu2 = histogram_of_u2.GetMean()
 RMSu2 = histogram_of_u2.GetRMS()
 VARu2 = RMSu2**2
 canv.cd(4)
+histogram_of_u10.Draw()
+canv.cd(5)
 histogram_of_un.Draw()
 MEANun = histogram_of_un.GetMean()
 RMSun = histogram_of_un.GetRMS()
 VARun = RMSun**2
+
+
 
 print ("MEANx1 = %.2f\nMEANx2 = %.2f\nMEANu2 = %.2f\nMEANun = %.2f\n"%( MEANx1, MEANx2, MEANu2, MEANun))
 print ("VARx1 = %.2f\nVARx2 = %.2f\nVARu2 = %.2f\nVARun = %.2f\n"%( VARx1, VARx2, VARu2, VARun))
