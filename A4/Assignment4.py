b3e17cbf7fdee790699ee8a5b31bf92b15a9aa1f
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2021-09-23 14:41:54 +0200

    u

--- A4/Assignment4.py
+++ A4/Assignment4.py
@@ -173,7 +173,7 @@ Particles = []
 p = Particle() #Generate first photon
 p.kind = 1
 p.energy = 100000  #in MeV
-p.start_pos =  ROOT.TVector3( 0,0,200000 ) #0,0,startheight
+p.start_pos =  ROOT.TVector3( 0,0,100000 ) #0,0,startheight
 theta       = 0.0001
 phi         = ROOT.gRandom.Rndm() * 2 * pi
 p.direction = direction_at_angle( ROOT.TVector3(0,0,-1), theta, phi )
@@ -200,4 +200,4 @@ for i in range(MaxGen) :
     if EndOfShower==0 : break #Stop the loop when all particles are below 85 MeV
     Generations.append(NewParticles)
 
-plot_shower(Generations)
+plot_shower(Generations, "Best Title ever", 10, 100000)
