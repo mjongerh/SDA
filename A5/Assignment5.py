f2c3f98100ac2fa940cd6cb2669456aa75f3e24f
mjongerh <mjongerh@live.nl>
2021-10-09 20:20:12 +0200

    s

--- A5/Assignment5.py
+++ A5/Assignment5.py
@@ -68,7 +68,7 @@ for i in range(len(bRange)):
     for j in range(len(aRange)):
         test = Chi2Test(mList, yList, aRange[j], bRange[i], BinWidthList)
         print(test)
-        hABchi2.SetBinContent(int(aRange[j]), int(bRange[i]), float(test))
+        hABchi2.SetBinContent(int(aRange[j]), int(bRange[i]), 1.123)
         othertest = hABchi2.GetBinContent(int(aRange[j]), int(bRange[i]))
         print(othertest)
 
