ec89f4c587faad88c297402a652bf281dd95ce80
mjongerh <mjongerh@live.nl>
2021-10-09 19:17:53 +0200

    s

--- A5/Assignment5.py
+++ A5/Assignment5.py
@@ -14,7 +14,7 @@ def Chi2Test (m, y, a, b, binwidth) : #For function am+b
     chi2 = 0
     for i in range(len(m)) :
         mui = (a*m[i-1]+b) #*binwidth[i]
-        chi2 += ((y[i-1]-mui)**2) / (mui**2)
+        chi2 += ((y[i-1]-mui)*(y[i-1]-mui)) / (mui*mui)
         print(mui)
         print(chi2)
     return chi2
