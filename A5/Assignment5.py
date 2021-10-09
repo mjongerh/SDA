0be0e0a4805656f039940e20c6319a029802b7b8
mjongerh <mjongerh@live.nl>
2021-10-09 15:00:40 +0200

    s

--- A5/Assignment5.py
+++ A5/Assignment5.py
@@ -3,6 +3,6 @@ import numpy
 from array import array
 from math import *
 
-infile = ROOT.TFile('assigment5-dataset.root')
+infile = ROOT.TFile('assignment5-dataset.root')
 hData = infile.Get('hdata')
 hData.Draw()
