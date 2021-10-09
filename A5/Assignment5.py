5a3215b5f402b5d8d8f7b7be5aba7ce19d9bf28c
mjongerh <mjongerh@live.nl>
2021-10-09 20:40:52 +0200

    s

--- A5/Assignment5.py
+++ A5/Assignment5.py
@@ -7,7 +7,7 @@ from math import *
 # Global Settings
 ################
 bRange = numpy.linspace(1.0, 5.0, 100) #range over which to test b
-aRange = numpy.linspace(-0.1, 0.0, 100) #range over which to test a
+aRange = numpy.linspace(-0.01, 0.0, 100) #range over which to test a
 
 ################
 # Global Functions
