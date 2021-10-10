1eebabb3cbd02f70734e9cbb3d8e8fc08b9e6867
mjongerh <mjongerh@live.nl>
2021-10-10 15:17:59 +0200

    s

--- A5/Assignment5.py
+++ A5/Assignment5.py
@@ -20,26 +20,100 @@ aRange = numpy.linspace(-0.0085, -0.0041, 100) #range over which to test a
 ################
 # Global Functions
 ################
-def Chi2Test (m, y, a, b, binwidth) : #For function am+b
-    chi2 = 0
-    index = 0
-    while index < len(m) :
-        mui = (a*m[index]*binwidth[index]+b) #*binwidth[index]
-        chi2 += ((y[index]-mui)) / (mui)
-        #print(mui)
-        #print(chi2)
-        index += 1
-    return chi2
-
-def LogLikelihood (m, y, a, b, binwidth) : #For function am+b
+def LogLikelihood (a, b) : #For function am+b
     LogL = 0
     index = 0
-    while index < len(m) :
-        mui = (a*m[index]+b) #*binwidth[index]
-        LogL += ((y[index]-mui)**2) / (2*mui**2)
-        #LogL += log(abs(mui))
+    while index < len(mList) :
+        mui = (a*mList[index]+b)
+        LogL += ((yList[index]-mui)**2) / (2*mui**2)
         index += 1
     return LogL
+
+def minimize( function,
+              start_values = None,
+              ranges       = None,
+              verbose      = False,
+              maxcalls     = 10000,
+              tollerance   = 1e-6 ):
+
+    args = inspect.getfullargspec( function )[0]  # list of functions arguments
+    npar = len(args)
+    
+    if not start_values : start_values = [0.0] * npar
+    if not ranges       : ranges       = [[0,0]] * npar
+
+    step_sizes   = [0.1] * npar
+    
+    minuit = ROOT.TMinuit( npar )
+
+
+    # set parameter names, start_values, ...
+
+    for i in range(npar) :
+        ierflg = ctypes.c_int()
+        minuit.mnparm( i, args[i] , start_values[i] , step_sizes[i], ranges[i][0], ranges[i][1], ierflg )
+        
+
+    def fcn( _npar, gin, f, par, iflag ):
+        "Wrapper around function, to provide signature expected by minuit."
+
+        # the buffer objects we get passed in behave strangly/buggy ->
+        # dont use _npar (but npar instead), and unpack par by hand:
+        parlist = [ par[i] for i in range (npar) ] 
+
+        f[0] = function( *parlist )
+
+        if verbose :
+            strpars = ",".join( str(x) for x in parlist )
+            print ("evaluating %s(%s) = %g " % (function.__name__, strpars, f[0] ))
+
+            
+    # call migrad
+
+    minuit.SetFCN( fcn ) # tell minuit to use our wrapper
+    arglist = array.array( 'd', [maxcalls, tollerance] )
+    minuit.mnexcm( "MIGRAD", arglist , len(arglist) , ierflg )
+
+    # return a list of fitted values
+    
+    r, errs = [],[]
+    for i in range( npar) :
+        r.append( ctypes.c_double() )
+        errs.append( ctypes.c_double() ) 
+        minuit.GetParameter( i, r[-1], errs[-1] )
+        
+    return r
+
+
+def fill_hist( func , nbinsx = 60, xmin = -5, xmax =5, nbinsy = 60, ymin= -5, ymax = 5 ):
+    
+    """ Helper funtion for plotting 2d-functions. Fill a root 
+        TH2D with the values from a 2d function. """
+    
+    ROOT.gStyle.SetPalette( 1 ) # pretty colors
+    
+    h2 = ROOT.TH2D( 'h', func.__name__ , nbinsx, xmin, xmax, nbinsy, ymin, ymax )
+    
+    # loop over all the bins and assing the z-value exactly
+    # once for each bin.
+
+    for bx in range( 1, h2.GetNbinsX()+1 ):
+        for by in range( 1, h2.GetNbinsY() +1 ):
+            
+            x = h2.GetXaxis().GetBinCenter( bx )
+            y = h2.GetYaxis().GetBinCenter( by )        
+            b = h2.GetBin( bx, by ) # global bin number
+
+            z = func(x,y)
+            h2.SetBinContent( b, z )
+
+
+    args = inspect.getfullargspec( func )[0]  # list of functions arguments
+    print ("args=",args)
+    h2.SetXTitle( args[0] )
+    h2.SetYTitle( args[1] )
+    return h2
+
 
 ################
 # Assignment a
@@ -56,7 +130,7 @@ bArray = array( 'd' )
 bFlatRange = numpy.linspace(3.0, 7.0, 100) #range over which to test b
 
 for i in range(len(bFlatRange)):
-    LbFit.append(LogLikelihood(mList, yList, 0, bFlatRange[i], BinWidthList))
+    LbFit.append(LogLikelihood(0, bFlatRange[i]))
     bArray.append(bFlatRange[i])
 
 
@@ -79,7 +153,7 @@ j = 0
 while i < len(bRange):
     j = 0
     while j < len(aRange):
-        test = LogLikelihood(mList, yList, aRange[j], bRange[i], BinWidthList)
+        test = LogLikelihood(aRange[j], bRange[i])
         #print(test)
         hABLogL.SetBinContent(j, i, test)
         #othertest = hABLogL.GetBinContent(j, i)
@@ -128,13 +202,18 @@ CanvABLogLcontrour.Update()
 
 CanvDataFitL = ROOT.TCanvas("CanvDataFitL", "Data with a fit y=am+b", 1000, 1000)
 #hData.Fit('pol1')
-FitFunc = ROOT.TF1("FitFunc", "-0.00589*x+7.29", 100, 1000) 
-FitFuncmin = ROOT.TF1("FitFuncmin", "-0.00589*x+7.29", 100, 1000) 
-FitFuncmax = ROOT.TF1("FitFuncmax", "-0.00589*x+7.29", 100, 1000) 
+FitFunc = ROOT.TF1("FitFunc", "-0.00589*x+7.29", 100, 1000)
 #best fit -0.00589m + 7.29
 #-0.00449 6.248
 #-0.00749  +8.67885
 # a= -0.00589 +-.00160
-# b = 7.46 +-1.39 
+# b = 7.29 +-1.39 
 hData.Draw()
 FitFunc.Draw("same")
+
+
+################
+# Assignment c
+################
+AutoFitResult = minimize(LogLikelihood, verbose = True)
+print ("Log Likelihood is minimal at ", AutoFitResult)
